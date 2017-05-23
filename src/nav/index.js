1049bb22cfbc5fa4a3f1e501d43d1bf0cca7a380
rudasn <rudasn@gmail.com>
2017-05-23 15:08:53 +0300

    navigation

--- src/nav/index.js
+++ src/nav/index.js
@@ -3,6 +3,9 @@ import Navbar from 'react-bootstrap/lib/Navbar'
 import { NavLink, } from 'react-router-dom'
 
 import './style.css'
+import FB from '../images/fb.svg'
+import Twitter from '../images/twitter.svg'
+import Instagram from '../images/instagram.svg'
 
 export default class TopNavBar extends PureComponent {
     render () {
@@ -29,9 +32,17 @@ export default class TopNavBar extends PureComponent {
                         <li>
                             <NavLink to="/about">About</NavLink>
                         </li>
-                        <li>
-                            <a href="http://abr.ticket.com.cy/event/page/1" target="_blank" rel="noopener noreferrer">Tickets</a>
+                        <li className="tickets">
+                            <a href="http://abr.ticket.com.cy/event/page/1"
+                               className="btn btn-danger"
+                               target="_blank"
+                               rel="noopener noreferrer">
+                               Tickets
+                            </a>
                         </li>
+                        <li className="social"><a href="https://www.facebook.com/TheAfroBananaRepublic" target="_blank" rel="noopener noreferrer"><img src={ FB } alt=""/></a></li>
+                        <li className="social"><a href="https://twitter.com/afrobananarpb" target="_blank" rel="noopener noreferrer"><img src={ Twitter } alt=""/></a></li>
+                        <li className="social"><a href="https://www.instagram.com/afrobananarpbc/" target="_blank" rel="noopener noreferrer"><img src={ Instagram } alt=""/></a></li>
                     </ul>
                 </Navbar.Collapse>
             </Navbar>
