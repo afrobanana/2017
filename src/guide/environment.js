1bb0beef44f9ec2bb02acf153094861aa5f1d930
rudasn <rudasn@gmail.com>
2017-05-23 15:08:30 +0300

    guide

--- src/guide/environment.js
+++ src/guide/environment.js
@@ -5,18 +5,20 @@ import envImg from './images/health-safety.png'
 export default () =>
     <section className="guide-environment">
         <img src={ envImg } alt="Take care of your selves!"/>
-        <h2>MEDICAL HELP & EMERGENCIES</h2>
-        <p>We have ensured that a team of professional first aid doctors/medics will be patrolling the site at all times to provide any help needed in case of emergency. Please contact your nearest security guard if help is required.</p>
+        <dl>
+            <dt>MEDICAL HELP & EMERGENCIES</dt>
+            <dd>We have ensured that a team of professional first aid doctors/medics will be patrolling the site at all times to provide any help needed in case of emergency. Please contact your nearest security guard if help is required.</dd>
 
-        <h2>FIRE SAFETY</h2>
-        <p>Fire hazards are no small deal during the summer months and we wouldn't want to burn the lovely forest down, so please be aware that lighting fire is strictly forbidden on the festival premises!</p>
+            <dt>FIRE SAFETY</dt>
+            <dd>Fire hazards are no small deal during the summer months and we wouldn't want to burn the lovely forest down, so please be aware that lighting fire is strictly forbidden on the festival premises!</dd>
 
-        <h2>CLEANLINESS & RECYCLING</h2>
-        <p>We kindly ask you to pay the utmost respect to the nature around you at the festival. Please leave no traces behind, keep the site clean, and separate your rubbish as the ABR team have made special arrangements for recycling after the festival. </p>
+            <dt>CLEANLINESS & RECYCLING</dt>
+            <dd>We kindly ask you to pay the utmost respect to the nature around you at the festival. Please leave no traces behind, keep the site clean, and separate your rubbish as the ABR team have made special arrangements for recycling after the festival. </dd>
 
-        <h2>LITTER & DESIGNATED BINS</h2>
-        <p>Labelled bins will be clearly marked on site. Please throw all litter in the designated bins, and not behind the trees, in the forest or anywhere else. Bins will be located all around the festival grounds, with distinct signs for rubbish separation. Please take special care to throw your cigarette butts in the designated bins and NOT on the beautiful festival grounds.</p>
+            <dt>LITTER & DESIGNATED BINS</dt>
+            <dd>Labelled bins will be clearly marked on site. Please throw all litter in the designated bins, and not behind the trees, in the forest or anywhere else. Bins will be located all around the festival grounds, with distinct signs for rubbish separation. Please take special care to throw your cigarette butts in the designated bins and NOT on the beautiful festival grounds.</dd>
 
-        <h2>DRINKING WITHOUT WASTE</h2>
-        <p>In an attempt to eliminate waste, reusable collectible branded cups (40cl) will be available at the festival for 1 euro.</p>
+            <dt>DRINKING WITHOUT WASTE</dt>
+            <dd>In an attempt to eliminate waste, reusable collectible branded cups (40cl) will be available at the festival for 1 euro.</dd>
+        </dl>
     </section>
