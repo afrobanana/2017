644b84283feb8519df9463787c0f0b716a3d9d69
rudasn <rudasn@gmail.com>
2017-05-24 16:28:51 +0300

    modal scroll

--- src/EntityModal.js
+++ src/EntityModal.js
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Col from 'react-bootstrap/lib/Col'
 import Grid from 'react-bootstrap/lib/Grid'
 import Modal from 'react-bootstrap/lib/Modal'
 import Row from 'react-bootstrap/lib/Row'
 
-export default ({ title, body, previous, next, className, goBack }) => (
-    <Modal show={ true } onHide={ goBack } className={ className }>
-        <Modal.Header closeButton>
-            <Modal.Title>{ title }</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            { body }
-        </Modal.Body>
-        <Modal.Footer>
-            <Grid fluid style={{ padding: 0 }}>
-                <Row>
-                    <Col sm={6} style={{ textAlign: 'left' }}>
-                        <strong>&larr; { previous }</strong>
-                    </Col>
-                    <Col sm={6}>
-                        <strong>{ next } &rarr;</strong>
-                    </Col>
-                </Row>
-            </Grid>
-        </Modal.Footer>
-    </Modal>
-)
+export default class EntityModal extends PureComponent {
+    componentDidUpdate() {
+        const modal = document.querySelector('.modal')
+        modal.scrollTop = 0
+    }
+
+    render() {
+        const { title, body, previous, next, className, goBack } = this.props
+
+        return (
+            <Modal
+                show={ true }
+                keyboard={ false }
+                onHide={ goBack }
+                className={ className }>
+                <Modal.Header closeButton>
+                    <Modal.Title>{ title }</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    { body }
+                </Modal.Body>
+                <Modal.Footer>
+                    <Grid fluid style={{ padding: 0 }}>
+                        <Row>
+                            <Col sm={6} style={{ textAlign: 'left' }}>
+                                <strong>&larr; { previous }</strong>
+                            </Col>
+                            <Col sm={6}>
+                                <strong>{ next } &rarr;</strong>
+                            </Col>
+                        </Row>
+                    </Grid>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+}
+
