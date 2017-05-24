be272dd7f4e1fd044b69d07033606a003c3ffca4
rudasn <rudasn@gmail.com>
2017-05-24 04:01:38 +0300

    landing and styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, } from 'react-router-dom'
@@ -5,7 +7,6 @@ import { HashRouter as Router, } from 'react-router-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 
 ReactDOM.render(
--- src/landing/index.js
+++ src/landing/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import './style.css'
-import LandingImg from '../images/landing.jpg'
+import LandingImg from './images/landing.jpg'
+import PosterImg from './images/poster.jpg'
 
 export default () =>
     <div className="landing">
-        <img src={ LandingImg } alt=""/>
+        <img src={ LandingImg } alt="" className="hidden-xs"/>
+        <img src={ PosterImg } alt="" className="visible-xs"/>
     </div>
