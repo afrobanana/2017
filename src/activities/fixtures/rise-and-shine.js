e5f4c63963fdb27830f3c71de6caac61cde01d6b
marthabanana <marthag@afrobananarepublic.com>
2017-05-25 16:47:15 +0300

    Update rise-and-shine.js

--- src/activities/fixtures/rise-and-shine.js
+++ src/activities/fixtures/rise-and-shine.js
@@ -5,7 +5,7 @@ export const photo = image
 // Markdown fields
 export const name = 'Rise and Shine Yoga'
 export const text = `
-Get up bright and early in the forest land to use breath and movement on a journey that will awaken your body and mind! With a yoga session, suitable for adults of all levels led by Nina Hadjidemetriou, prepare to start off with a cleansing pranayama practice, then moving through a smooth warm-up, before you energize and ground yourselves with a standing sequence and then unwinding with some balancing and seated asanas. It will all draw to a close with a relaxation and a short meditation inviting gratitude and joy for the day. ‘Rise and Shine’ yoga will be taking place on both the Friday and Saturday of the festival.
+Get up bright and early in the forest to use breath and movement on a journey that will awaken your body and mind! With a yoga session, suitable for adults of all levels led by Nina Hadjidemetriou, prepare to start off with a cleansing pranayama practice, then move through a smooth warm-up, before you energize and ground yourselves with a standing sequence and then unwind with some balancing and seated asanas. It will all draw to a close with a relaxation and a short meditation inviting gratitude and joy for the day. ‘Rise and Shine’ yoga will be taking place on both Saturday and Sunday at the festival.
 
 **Capacity:** 12 people (bring your own mat)
 
