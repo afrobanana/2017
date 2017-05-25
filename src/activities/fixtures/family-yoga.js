a46b454de2c2b015d7eb896b37d79c56aa67ca26
marthabanana <marthag@afrobananarepublic.com>
2017-05-25 16:40:07 +0300

    Update family-yoga.js

--- src/activities/fixtures/family-yoga.js
+++ src/activities/fixtures/family-yoga.js
@@ -5,9 +5,9 @@ export const photo = image
 // Markdown fields
 export const name = 'Bonding through Family Yoga'
 export const text = `
-Ready for a real bonding experience for the whole family? It’s almost time for an educational and entertaining yoga inspired workshop to strengthen the trust and the connection between family members. So go on parents...unleash your child-like spirit as you play, practice and relax with your kids. The workshop will be led by Stavri Ioannou.
+Ready for a real bonding experience for the whole family? It’s almost time for an educational and entertaining yoga-inspired workshop to strengthen the trust and the connection between family members. So go on parents...unleash your child-like spirit as you play, practise and relax with your kids. The workshop will be led by Stavri Ioannou.
 
-**Capacity:** 8-10 families of at least one parent and one kid
+**Capacity:** 8-10 pairings of one adult and one child
 
 **Duration:** 1 hour
 
