00e42c6fe9a224e6dbfac2b205c99bd958223f18
marthabanana <marthag@afrobananarepublic.com>
2017-05-25 16:43:14 +0300

    Update writecy.js

--- src/activities/fixtures/writecy.js
+++ src/activities/fixtures/writecy.js
@@ -5,11 +5,11 @@ export const photo = image
 // Markdown fields
 export const name = 'Write CY Open Mic Night'
 export const text = `
-Community storytelling is experiencing a revival worldwide. And it isn’t hard to see why! Sharing stories before live audiences is an antidote to our increasingly virtual existences, making our communities more connected and lively places. Write Cy is now staging an Open Mic Night at the Afro Banana Festival, inviting all creative types to perform, as long as you have original material and can perform it in under five minutes.
+Community storytelling is experiencing a revival worldwide. And it isn’t hard to see why! Sharing stories before live audiences is an antidote to our increasingly virtual existences, making our communities more connected and lively places. Write Cy is now staging an Open Mic Night at the ABR Festival, inviting all creative types to perform, as long as you have original material and can perform it in under five minutes.
 
 The Nicosia-based creative writing space recently introduced live storytelling in Cyprus, drawing a local community of local storytellers, poets and provocateurs who gather at Prozak Kafeneio once a month to tell their stories.
 
-To find out more about Write CY’s Open Mic Nights and to **register to perform** at the Afro Banana 2017, check our website:
+To find out more about Write CY’s Open Mic Nights and to **register to perform** at the ABR Festival 2017, check our website:
 
 <a href="http://www.writecy.com/open-mic" target="_blank" rel="noopener noreferrer" class="btn btn-info">
     www.writecy.com/open-mic
