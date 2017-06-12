ddea8d6c77f8bcfc80be5eba4251e2c12ebc9455
rudasn <rudasn@gmail.com>
2017-06-12 20:03:27 +0300

    youth inspire

--- src/activities/fixtures/youth-inspire.js
+++ src/activities/fixtures/youth-inspire.js
@@ -5,7 +5,7 @@ export const photo = image
 // Markdown fields
 export const name = 'Youth-Inspire!'
 export const text = `
-Listen up! Prepare to be seriously inspired with a series of talks by young speakers invited to the festival to provoke and intrigue you. More information about the speakers will be shared via the ABR Festival’s social media pages and on location. This event is supported by Eramus+
+Listen up! Prepare to be seriously inspired with a series of talks by young speakers invited to the festival to provoke and intrigue you. More information about the speakers will be shared via the ABR Festival’s social media pages and on location. This event is supported by Eramus+: Youth Cyprus National Agency.
 
 **Capacity:** No limits
 
